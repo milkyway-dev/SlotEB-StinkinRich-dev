c570717ff713ae885cb81b42acabf7818e049a29
Mehul Parakh <mehul@underpinservices.com>
2025-01-29 16:43:50 +0530

    FIX: Low balance popup will not open up on changing bet

--- Assets/Scripts/Functionality/SlotBehaviour.cs
+++ Assets/Scripts/Functionality/SlotBehaviour.cs
@@ -411,7 +411,7 @@ public class SlotBehaviour : MonoBehaviour
         if (LineBet_text) LineBet_text.text = SocketManager.initialData.Bets[BetCounter].ToString();
         if (TotalBet_text) TotalBet_text.text = (SocketManager.initialData.Bets[BetCounter] * Lines).ToString();
         currentTotalBet = SocketManager.initialData.Bets[BetCounter] * Lines;
-        CompareBalance();
+        // CompareBalance();
     }
 
     private void ToggleTotalBet(bool IncDec)
